402cd0529a8ce1155e3488cf91ac030ec8687992
shmulik-bn <shmulikbennatan@gmail.com>
2022-07-02 18:25:27 +0300

    first commit

---
+++ entities/JobInfo.js
@@ -0,0 +1,36 @@
+//מיון
+
+
+class JobInfo {
+
+    constructor(data){
+        try {
+            this.link = data.entities[1].url;
+        this.job = this.findText(data.text, 0, "at");
+
+        // this.location = this.findText(data.text, "Location", "\n");
+        // this.company = this.findText(data.text, "at", "\n");
+        } catch (error) {
+            console.log('error in class JobInfo:', error);
+        }
+    };
+    job;
+    location;
+    company;
+    link;
+    // languages;//[]
+    // sendingTimeStamp;
+    // experience;
+
+    findText = (data, keyStart, keyEnd) =>{
+        let indexOfProp = keyStart? data.search(keyStart) : keyStart;
+        if(indexOfProp !== -1){
+            let subStr = data.slice(indexOfProp, data.length);
+            let subStringEndIndex = subStr.search(keyEnd);
+            return subStr.substring(indexOfProp, subStringEndIndex);
+
+        }
+        else return;
+    }
+}
+module.exports = JobInfo;
---
+++ index.js
@@ -0,0 +1,58 @@
+const TelegramBot = require('node-telegram-bot-api');
+const JobInfo = require('./entities/JobInfo');
+const token = '5519820453:AAGYgae5r_pSIvHw3-TppIO0OF21bFY4iRE';
+
+const bot = new TelegramBot(token, {polling: true});
+const chatId = "-721901979";
+
+const keyWords = ["Full Stack","Fullstack", "Front End", "Frontend", "Back end", "backend"];
+
+
+
+const included = (text, keyWords) =>{
+    for(const w of keyWords){
+        if (text.search(w) !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+if(included(msg.text, keyWords)) {
+const jobInfo = new JobInfo(msg);
+console.log('jobInfo class instance:', jobInfo);
+bot.sendMessage(chatId, `job: ${jobInfo.job} \nlink: ${jobInfo.link}`);
+}
+
+    console.log('msg:', msg);
+
+  
+});
+
+
+
+
+// msg: {
+//     message_id: 20,
+//     from: {
+//       id: 325043125,
+//       is_bot: false,
+//       first_name: 'ש',
+//       last_name: 'ב',
+//       language_code: 'he'
+//     },
+//     chat: {
+//       id: -721901979,
+//       title: 'Job tests',
+//       type: 'group',
+//       all_members_are_administrators: true
+//     },
+//     date: 1656764203,
+//     text: 'Junior System Analyst at Sisense\n' +
+//       'Location: Tel Aviv\n' +
+//       'Press here to apply\n' +
+//       '#JuniorPositions',
+//     entities: [ { offset: 72, length: 16, type: 'hashtag' } ]
+//   }
