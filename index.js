ea4de3c33bec3bb089b1a1b1d91c0288ad971263
shmulik-bn <shmulikbennatan@gmail.com>
2022-07-02 18:30:34 +0300

    Add error handling

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ bot.sendMessage(chatId, `job: ${jobInfo.job} \nlink: ${jobInfo.link}`);
   
 });
 
-
+process.on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+  });
 
 
 // msg: {
